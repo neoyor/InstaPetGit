ac68fbb9f392ff1bf6e26c9639f18e42ef5d7b1a
Metanix <metanix.18@gmail.com>
2015-06-18 21:50:54 -0300

    cambio de prueba

---
+++ test/pruebas.js
@@ -0,0 +1,16 @@
+var should = require('should'),
+	supertest = require('supertest');
+	
+
+describe('camina', function(){
+
+	it('deberia caminar', function(done){
+		done();
+	});
+
+	it('no deberia caminar', function(done){
+		//throw "no pasa";
+		done();
+	});
+
+});
